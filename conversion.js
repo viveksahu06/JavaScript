3facd3b2fc2a242e628955fefa7d279e1f156b1e
viveksahu06 <viveksahu1006@gmail.com>
2024-01-15 23:40:11 +0530

    stage 2

---
+++ conversion.js
@@ -0,0 +1,19 @@
+// let score = "hii"
+// console.log(typeof score)
+// console.log(typeof score)
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber) /*NaN=> not a number */
+//     /*"33"=>33 
+//      "33abc"=>Nan
+//       true = 1 *
+//      false = 0 */
+let loggedIn = 1;
+let booleanIsLoggedIn = Boolean(loggedIn)
+console.log(booleanIsLoggedIn)
+    //1=> true;0=>false; 
+    //hitesh =true  
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
+console.log(stringNumber)
