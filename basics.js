c45faad3d8d2139ffccc86476d39c07b2b221af2
viveksahu06 <viveksahu1006@gmail.com>
2024-01-15 23:22:22 +0530

    datatype

---
+++ basics.js
@@ -0,0 +1,18 @@
+console.log("vivek sahu journey is started here");
+const accountId = 144;
+let accountEmail = "vinay@gmail.com";
+var accountPassword = "123456"
+accountCity = "jaipur";
+
+//accountId = 234
+/* if we declare a variable like a const we cant change the value throughtout the code
+ */
+var accountState;
+/*if we doesnt give any explicit value to the variable it gives that the undifined value of code */
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+accountEmail = "vinay@baba.com"
+accountPassword = "asdfjkl;"
+accountState = "andhra";
+accountCity = "Mp"
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
