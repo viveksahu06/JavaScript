d5cf736cc49f53523b0c77f0f7ab17443d317d68
viveksahu06 <viveksahu1006@gmail.com>
2024-01-15 23:23:48 +0530

    clear

---
+++ datatypes.js
@@ -0,0 +1,29 @@
+"use strict"
+/* this line state that we new syntax
+have to use the latest version and syntax of the languae */
+//alert("hello") //directly ant use we are using node js not browser
+console.log(34 +
+        3)
+    /* make use your code readable */
+console.log("vivek" + 3)
+
+
+
+let name = "hitesh"
+let age = 21
+let isLoggedIn = false
+
+//number>>2 to power 53
+//bigint
+//string=> " "
+//boolean=>true/false
+//null=>stand alone value
+//undifined=>value not assigned
+//symbol=>uniquesness
+
+
+//object
+console.log(typeof age)
+console.log(typeof null)
+    //nulll is a object
+console.log(typeof undefined)
